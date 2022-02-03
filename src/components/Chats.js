dec3a689c77a005d42d26655d0cade2ac09519d5
Armita Ahmadi <armitaahmadi.business@gmail.com>
2022-02-03 22:52:26 +0330

    context and logout

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import { auth } from '../firebase';
+import { useNavigate } from 'react-router-dom';
 
 // Components
 import Navbar from './Navbar';
 
 const Chats = () => {
+
+    const navigate = useNavigate();
+
+    const logoutHandler = async () => {
+        await auth.signOut();
+        navigate('/')
+    }
+
     return (
         <div>
-            <Navbar />
+            <Navbar logoutHandler={logoutHandler} />
         </div>
     );
 };
